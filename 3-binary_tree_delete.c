5c2f4c677b54819e25256938831ed59b5d542258
O-G-W-A-L <ogwalja04@gmail.com>
2024-02-27 01:39:37 -0800

    delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,9 +7,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-  if(!tree)
-    return;
-  binary_tree_delete(tree->right);
-  binary_tree_delete(tree->left);
-  free(tree);
+if(tree != NULL)
+{
+binary_tree_delete(tree->right);
+binary_tree_delete(tree->left);
+free(tree);
+}
 }
